9c00f5fcc5146a4427e8813bbc92f8b2ca0b3a4d
Tshephang <100064063+TSH3PH4NG@users.noreply.github.com>
2023-12-22 11:55:54 +0200

    First plugin

---
+++ plugins/Alive.js
@@ -0,0 +1,11 @@
+const {command} = require("lib");
+
+command({
+  pattern:"alive",
+  fromMe: false,
+  desc: "test alive"
+},
+async(message, m)=>{
+return message.send(message.jid, {text: `hello ${message.pushName} all systems are functional`}, {quoted: m})
+  
+})
