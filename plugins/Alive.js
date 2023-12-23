53472beb6f3eb02c269e90673dab1aa1f439812c
Tshephang <100064063+TSH3PH4NG@users.noreply.github.com>
2023-12-23 13:31:23 +0200

    Update Alive.js

--- plugins/Alive.js
+++ plugins/Alive.js
@@ -6,6 +6,6 @@ command({
   desc: "test alive"
 },
 async(message, m)=>{
-return message.client.sendMessage(message.jid, {text: `hello ${message.pushName} all systems are functional`}, {quoted: m})
+await message.client.sendMessage(message.jid, {text: `hello ${message.pushName} all systems are functional`}, {quoted: m})
   
 })
