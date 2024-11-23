68ade3b035a5a21438fffa097208b2a158f0f51b
マーティン 武蔵 <100064063+TSH3PH4NG@users.noreply.github.com>
2024-11-23 07:23:22 +0200

    Create user_mobile.js

---
+++ plugins/user_mobile.js
@@ -0,0 +1,24 @@
+const { getDevice } = require("@whiskeysockets/baileys");
+const {command} = require("../lib");
+
+
+command({
+pattern: "user",
+fromMe: true
+},
+async(message,match,m)=>{
+	
+let bb = await getDevice(message.reply_message.key.id)
+
+const name = {
+  "ios": "_IPHONE_",
+  "android": "android"
+};
+
+bb = name[bb];
+
+return message.client.sendMessage(message.jid , {text: `
+_USER-PHONE_: ${bb}
+`}, {quoted: m })
+
+})
