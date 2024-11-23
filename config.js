40a312f8e8da96b342cd96fd973f2fc29a5127dc
マーティン 武蔵 <100064063+TSH3PH4NG@users.noreply.github.com>
2024-11-23 07:30:57 +0200

    Update config.js

--- config.js
+++ config.js
@@ -29,11 +29,11 @@ module.exports = {
     GOODBYE_MSG: process.env.GOODBYE_MSG || 'Hi @user It was Nice Seeing you',
     AUTHOR: process.env.AUTHOR || 'Tshephang',
     SUDO: process.env.SUDO || '',
-    CR: toBool(process.env.CR) || "false",
+    CALL_REJECT: toBool(process.env.CALL_REJECT) || true,
     HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || '',
     HEROKU_API_KEY: process.env.HEROKU_API_KEY || '',
     OWNER_NAME: process.env.OWNER_NAME || 'Tshephang',
-    BOT_NAME: process.env.BOT_NAME || 'luna-md',
+    BOT_NAME: process.env.BOT_NAME || 'iris-md',
     WORK_TYPE: process.env.WORK_TYPE || 'private',
     DATABASE_URL: DATABASE_URL,
     DATABASE: DATABASE_URL === './resources/database.db'
