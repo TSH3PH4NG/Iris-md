c50ca2b404edb17eb5772140de30103c6d08619d
マーティン 武蔵 <100064063+TSH3PH4NG@users.noreply.github.com>
2025-01-06 09:56:11 +0200

    Create terminal.js

---
+++ plugins/terminal.js
@@ -0,0 +1,25 @@
+const { command }= require("../lib");
+const { exec } = require("child_process");
+
+command (
+    {
+        pattern: "run",
+        fromMe: true,
+        desc: "get linux machine",
+        type: "machine"
+    },
+    async (message, match,m) => {
+    	if(!match) return;
+        exec(match, (error, stdout, stderr) => {
+            if (error) {
+                return message.reply(`Error: ${error.message}`);
+            }
+
+            if (stderr) {
+                return message.reply(`Error: ${stderr}`);
+            }
+
+            return message.reply(`Output: ${stdout}`);
+        });
+    }
+);
