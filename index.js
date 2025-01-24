9b1b0208ad5391863319c1a36efb86202dd0e0d6
マーティン 武蔵 <100064063+TSH3PH4NG@users.noreply.github.com>
2025-01-24 19:56:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -133,9 +133,6 @@ async function Iris() {
                 const regex = new RegExp(`^${command.pattern.source}`);
                 const cmd = msg.body.match(regex);
                 comman = cmd && cmd[0]?.startsWith(prefix) ? cmd[0] : false; /*it's the same as if/else*/
-                console.log(`MESSAGE: ${text_msg}\ncmd: ${cmd}\ncomman: ${comman}\ncommand: ${command.pattern}`)
-            } else {
-                comman = false;
             }
 
             msg.prefix = prefix;
