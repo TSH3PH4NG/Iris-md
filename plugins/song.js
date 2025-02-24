3b8d5770253ad5a0386775e39399f78beb8c083f
Tshepang Masia <100064063+TSH3PH4NG@users.noreply.github.com>
2025-02-24 10:09:51 +0200

    Update song.js

--- plugins/song.js
+++ plugins/song.js
@@ -5,8 +5,7 @@ pattern: "song",
 fromMe: false
 },
 async(message,match, m)=>{
-if(!match) return message.client.sendMessage(message.jid , {text: "i need a query"}, {quoted: m})
-
+if(!match) return message.reply("_i need a query_");
 
 let { title , url } = await yts(match);
 let  buff  = await ytdl(url);
@@ -15,13 +14,14 @@ buff = await metaData(title, buff);
 try{
 return await message.client.sendMessage(message.jid, { audio: buff , mimetype: "audio/mpeg" }, {quoted: m });
 }catch(e){
+  await message.reply(e);
   
-let decoy = await tubis(title);
+/*let decoy = await tubis(title);
 let decoydl = await tubidl(decoy[0]?.link);
 let buffer = await getBuffer(decoydl[0]?.url);
 buffer = await metaData(title,buffer);
   
-return await message.client.sendMessage(message.jid, { audio: buffer , mimetype: "audio/mpeg"}, {quoted: m})
+return await message.client.sendMessage(message.jid, { audio: buffer , mimetype: "audio/mpeg"}, {quoted: m})*/
 }
 
 })
