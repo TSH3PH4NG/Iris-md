dc257d8571b1b47f4208429ace60945f679f2981
Tshepang Masia <100064063+TSH3PH4NG@users.noreply.github.com>
2025-03-02 14:03:50 +0200

    Update find.js

--- plugins/find.js
+++ plugins/find.js
@@ -1,50 +1,40 @@
-const { command , metaData , getBuffer , yts , ytdl , shazam } = require("../lib");
-const acrcloud = require("acrcloud")
+const { command, metaData, getBuffer, yts, ytdl, shazam } = require("../lib");
+const acrcloud = require("acrcloud");
 const fs = require("fs-extra");
 const ffmpeg = require('fluent-ffmpeg');
 
-command(
-{
-pattern : "find",
-fromMe: false,
-desc: "music finder"
- },
- 
- async (message, match, m) => {
- if (!message.reply_message.message.videoMessage && !message.reply_message.message.audioMessage)
-      return await message.reply("only works on videos and audio files");
-	
- let buff = await m.quoted.download()
-try{
- 	const acr = new acrcloud({
-  host: "identify-eu-west-1.acrcloud.com",
-  access_key: "df8c1cffbfa4295dd40188b63d363112",
-  access_secret: "d5mygczEZkPlBDRpFjwySUexQM26jix0gCmih389"
-});
+command({ pattern: "find", fromMe: false, desc: "music finder" }, async (message, match, m) => {
+    if (!message.reply_message.message.videoMessage && !message.reply_message.message.audioMessage)
+        return await message.reply("only works on videos and audio files");
 
+    let buff = await m.quoted.download();
 
-let res = await acr.identify(buff)
-let platform;
-let finder;
+    try {
+        const acr = new acrcloud({
+            host: "identify-eu-west-1.acrcloud.com",
+            access_key: "df8c1cffbfa4295dd40188b63d363112",
+            access_secret: "d5mygczEZkPlBDRpFjwySUexQM26jix0gCmih389"
+        });
 
-try{
-finder  = res.metadata.music[0].album?.name
-plaform = "acrcloud"
-} catch {
-	
-let metadata = await shazam(buff);
+        let res = await acr.identify(buff);
+        let platform;
+        let finder;
 
-if(metadata == "Track not found."){
-return message.reply("all attempts failed");
-};
+        try {
+            finder = res.metadata.music[0].album?.name;
+            platform = "acrcloud";
+        } catch {
+            let metadata = await shazam(buff);
+            if (metadata == "Track not found.") {
+                return message.reply("all attempts failed");
+            }
+            finder = metadata.title;
+            platform = "shazam";
+        }
 
-finder = metadata.title
-platform = "shazam";
-}
-    
-let { title , link , thumbnail , duration } = await yts(finder);
-let im = await getBuffer(thumbnail)
-    let  text = `
+        let { title, link, thumbnail, duration } = await yts(finder);
+        let im = await getBuffer(thumbnail);
+        let  text = `
 ╭━━〘 𝑀𝑈𝑆𝐼𝐶 𝐹𝐼𝑁𝐷𝐸𝑅 〙
 ┃ 
 ┠ title: ${title}
@@ -54,44 +44,38 @@ let im = await getBuffer(thumbnail)
 ┃ 
 ╰━━━━━━━━━━━──⊷`
 
- 	return await message.client.sendMessage(message.jid, {text: text, contextInfo:{ 
-externalAdReply:{
+        return await message.client.sendMessage(message.jid, {
+            text: text,
+            contextInfo: {
+                externalAdReply: {
+                    title: `𝐓𝐒𝐇𝐄𝐏𝐀𝐍𝐆 𝐌𝐀𝐑𝐓𝐈𝐍 𝐌𝐀𝐒𝐈𝐀`,
+                    mediaType: 1,
+                    previewType: 0,
+                    renderLargerThumbnail: true,
+                    thumbnail: im
+                }
+            }
+        }, { quoted: m });
 
-title: ` 𝐓𝐒𝐇𝐄𝐏𝐀𝐍𝐆 𝐌𝐀𝐑𝐓𝐈𝐍 𝐌𝐀𝐒𝐈𝐀 `,
-mediaType: 1,
-previewType: 0,
-renderLargerThumbnail: true,
-thumbnail: im
-                                 }
+    } catch (e) {
+        message.reply(e);
+    }
+});
 
+command({ on: "text", fromMe: false }, async (message, match, m) => {
+    if (match == 1 && m.quoted.text.includes("url")) {
+        try {
+            let final = m.quoted.text.split("┠ ")[2];
+            final = final.replace("url:", "");
+            let ur_l = final;
+            let data = await ytdl(ur_l);
+            await message.client.sendMessage(message.jid, {
+                audio: data,
+                mimetype: "audio/mpeg"
+            }, { quoted: m });
+        } catch (e) {
+            return e;
+        }
+    }
+});
 
-
-}}, {quoted: m})
-
- }catch(e){
-message.reply(e)
-}
- 
-})
-
-command({
- on: "text",
- fromMe: false
-},
-async(message,match ,m)=> {
-if(match == 1 && m.quoted.text.includes("url")){
-		
-try{
-
-let final = m.quoted.text.split("┠ ")[2] 
-final = final.replace("url:", "")
-let ur_l = final;
-let data = await ytdl(ur_l);
-	
-await message.client.sendMessage(message.jid , {audio: data , mimetype: "audio/mpeg"} ,{quoted: m})
-
-}catch(e){
-return e
-}
-}
-})
