96cc03da1949b6a434b31514cbaf9ce3f70e56be
Tshepang Masia <100064063+TSH3PH4NG@users.noreply.github.com>
2025-03-04 00:30:27 +0200

    Update song.js

--- plugins/song.js
+++ plugins/song.js
@@ -7,21 +7,13 @@ fromMe: false
 },
 async(message,match, m)=>{
 if(!match) return message.reply("_i need a query_");
-
+try{
+  
 let { title , url } = await (await yts(match)).all[0]
 let  buff  = await ytdl(url);
-
-try{
+  
 return await message.client.sendMessage(message.jid, { audio: buff , mimetype: "audio/mpeg" }, {quoted: m });
 }catch(e){
   await message.reply(e);
-  
-/*let decoy = await tubis(title);
-let decoydl = await tubidl(decoy[0]?.link);
-let buffer = await getBuffer(decoydl[0]?.url);
-buffer = await metaData(title,buffer);
-  
-return await message.client.sendMessage(message.jid, { audio: buffer , mimetype: "audio/mpeg"}, {quoted: m})*/
-}
-
+ }
 })
