58381030af0525096f4b15814a5cc344720ce6fd
TSH3PH4NG <100064063+TSH3PH4NG@users.noreply.github.com>
2025-08-06 21:45:09 +0200

    optimised

--- plugins/song.js
+++ plugins/song.js
@@ -11,12 +11,13 @@ async(message,match, m)=>{
 if(!match) return message.reply("_i need a query_");
 try{
   
-let { title , url } = await ( await axios(`https://tshepang-yasuke-martin.hf.space/yts?q=${match}`)).data[0];
-let  buff  = await ytdl(url);
-if(!Buffer.isBuffer(buff)) return; /*idk what to log so this will do for now*/
-buff = await metaData(title, buff);
-  
-return await message.client.sendMessage(message.jid, { audio: buff , mimetype: "audio/mpeg" }, {quoted: m });
+let { title , url } = await ( await axios(`https://api-ij32.onrender.com/yts?q=${match}`)).data[0];
+let  doc_url  = await ytdl(url);
+
+/*if(!Buffer.isBuffer(buff)) return;
+buff = await metaData(title, buff);*/
+
+return await message.client.sendMessage(message.jid, { document: { url: doc_url }, mimetype: "audio/mpeg" , fileName: `${title}.mp3`}, { quoted: m });
 }catch(e){
   await console.log(e);
  }
