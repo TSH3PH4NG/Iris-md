eb0b7d67295b94287e79ca6d9b11b668c5e58dcd
Tshepang Martin Masia‎ <100064063+TSH3PH4NG@users.noreply.github.com>
2025-12-09 10:33:46 +0200

    don't mind the command naming

--- plugins/song.js
+++ plugins/song.js
@@ -2,7 +2,29 @@ const { command , ytdl , getBuffer , metaData }  = require("../lib");
 const axios = require("axios");
 
 command({
-pattern: "song",
+pattern: "yx",
+fromMe: false,
+desc: "Download songs",
+type: "download"
+},
+async(message,match, m)=>{
+if(!match) return message.reply("_i need a query_");
+try{
+  
+
+let { url } = await (await axios(`https://api-ij32.onrender.com/yts?q=${match}`)).data[0];
+let  final  = await ytdl(url);
+
+return await message.client.sendMessage(message.jid, { audio: { url: final }, mimetype: "audio/mpeg" }, { quoted: m });
+
+}catch(e){
+  await console.log(e);
+ }
+ 
+});
+
+command({
+pattern: "xy",
 fromMe: false,
 desc: "Download songs",
 type: "download"
@@ -18,6 +40,39 @@ let  doc_url  = await ytdl(url);
 buff = await metaData(title, buff);*/
 
 return await message.client.sendMessage(message.jid, { document: { url: doc_url }, mimetype: "audio/mpeg" , fileName: `${title}.mp3`}, { quoted: m });
+}catch(e){
+  await console.log(e);
+ }
+})
+
+command({
+pattern: "song",
+fromMe: false,
+desc: "Download songs",
+type: "download"
+},
+async(message,match, m)=>{
+if(!match) return message.reply("_i need a query_");
+try{
+  
+let { title , url , thumbnail } = await ( await axios(`https://api-ij32.onrender.com/yts?q=${match}`)).data[0];
+
+await message.client.sendButton(message, { image: { url: thumbnail } , caption: `Song: ${title}`,
+footer: "Made with 🫶 by Tshepang",
+buttons: [{
+	text: "audio",
+	id: `.yx  ${url}`,
+},{
+	text: "document",
+	id: `.xy  ${url}`,
+},{
+	options: {
+		text: "copy_url",
+		copy_code: url,
+	}
+}]
+}, { quoted: m })
+
 }catch(e){
   await console.log(e);
  }
