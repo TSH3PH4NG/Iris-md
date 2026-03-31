6f5364a5b00de9f5c48bfa897eb3bfb8529aed3b
Tshepang Masia‎ <100064063+TSH3PH4NG@users.noreply.github.com>
2026-03-31 22:20:32 +0200

    Create alive.js

---
+++ plugins/alive.js
@@ -0,0 +1,17 @@
+const { command , uptime } = require("../lib");
+
+command({
+  pattern:"alive",
+  fromMe: false,
+  type: "misc",
+  desc: "Bot status"
+},
+async(message , m , match)=>{
+let bb = await uptime(process.uptime())
+return message.reply(`
+Hello ${message.pushName} all systems are functional
+uptime: ${bb}
+`)
+  
+})
+
